918d2d417ace3ac90ca06bd8bacbe67b1bc58597
96olbap <alvinwanjala@gmail.com>
2023-05-14 22:01:54 +0300

    This commit creates the client stub

--- client.cpp
+++ client.cpp
@@ -14,3 +14,33 @@ class StudentRegistration {
 public:
   void registerStudent(const std::string& studentDetails);
 };
+
+// Client stub
+class StudentRegistrationClient {
+public:
+  void registerStudent(const std::string& studentDetails) {
+    // Open a TCP connection to the server
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // assuming server is running on the same machine | if on separate machines change the addr
+    servaddr.sin_port = htons(8080);
+    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    // Send the request to the server
+    std::string request = "register " + studentDetails;
+    send(sockfd, request.c_str(), request.length(), 0);
+
+    // Receive the reply from the server
+    char buffer[1024];
+    int n = recv(sockfd, buffer, sizeof(buffer), 0);
+    buffer[n] = '\0';
+    std::string reply(buffer);
+
+    // Print the reply
+    std::cout << reply << std::endl;
+
+    // Close the connection
+    close(sockfd);
+  }
+};
