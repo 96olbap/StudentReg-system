0f9c4c34d694ea700e3c3da67221adbe377e37f1
96olbap <alvinwanjala@gmail.com>
2023-05-14 20:58:31 +0300

    this commit binds the socket to the server address

--- server.cpp
+++ server.cpp
@@ -30,4 +30,11 @@ public:
 int main() {
     // Create a TCP socket
     int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //Bind the socket to the server address
+    struct sockaddr_in servaddr;
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(8080);
+    bind(listenfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 }
